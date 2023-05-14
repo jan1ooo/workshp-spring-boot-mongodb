3e01e67c982305a43c7cf7305e8e545034a940c6
jan1ooo <filhojanio67@gmail.com>
2023-05-13 22:17:22 -0300

    First commit - projeto criado

---
+++ src/main/java/com/jan1ooo/workshopmongo/WorkshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.jan1ooo.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoApplication.class, args);
+	}
+
+}
